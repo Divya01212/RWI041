57212f54d1da6bfe6405f1086898146d396ead6c
Divya01212 <165903847+Divya01212@users.noreply.github.com>
2024-04-29 15:03:54 +0530

    Add files via upload

---
+++ StringBuffer/Reverse.java
@@ -0,0 +1,12 @@
+package StringBuffer;
+
+public class Reverse {
+
+	public static void main(String[] args) {
+		StringBuffer a= new StringBuffer("Divya Mishra");
+		System.out.println(a.reverse());
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ StringBuilder/Reverse.java
@@ -0,0 +1,12 @@
+package StringBuilder;
+
+public class Reverse {
+
+	public static void main(String[] args) {
+		StringBuilder r= new StringBuilder("radhika gupta");
+		System.out.println(r.reverse());
+		// TODO Auto-generated method stub
+
+	}
+
+}
